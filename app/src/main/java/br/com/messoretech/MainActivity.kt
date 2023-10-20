933315a7d263f811d41e43b4da8baacf38913e08
GiovanaVari <giovana.vari.de.assis@gmail.com>
2023-10-20 19:21:00 -0300

    App test material design

---
+++ app/src/main/java/br/com/messoretech/MainActivity.kt
@@ -0,0 +1,51 @@
+package br.com.messoretech
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.messoretech.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.cvButtons.setOnClickListener {
+            startActivity(ButtonsActivity.createIntent(this))
+        }
+
+        binding.cvTextFields.setOnClickListener {
+            startActivity(TextFieldActivity.createIntent(this))
+        }
+
+        binding.cvBottomSheets.setOnClickListener {
+            ModalBottomSheet.start(supportFragmentManager)
+        }
+
+        binding.cvTopAppBar.setOnClickListener {
+            startActivity(TopAppBarActivity.createIntent(this))
+        }
+
+        binding.cvBottomAppBar.setOnClickListener {
+            startActivity(BottomAppBarActivity.createIntent(this))
+        }
+
+        binding.cvSnackBar.setOnClickListener {
+            startActivity(SnackbarActivity.createIntent(this))
+        }
+
+        binding.cvBottomNavigation.setOnClickListener {
+            startActivity(BottomNavigationActivity.createIntent(this))
+        }
+
+    }
+}
